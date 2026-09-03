796082408c7b2e6e557ca5c17bad8d97837fda6e
Caue Tavares <caue.t.silva@edu.senai.br>
2026-09-03 17:49:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,58 +1,21 @@
+//DOCUMENT   --> PAGINA INTEIRA DO MEU HTML 
+// getElementByID -> procura pelo atributo id=""
 
-Exercicios Nivel Basico
+// titulo-filme -> ID que queremos. 
 
-1 - 
-    for (let i = 1; i <= 10; i++) {
-  console.log(i);
-}
+//const elemento = document.getElementById('filme-principal')
+//console.log(elemento)
+//console.log(elemento.tagName)
 
-2 - 
-readline.question('Digite um número: ', (input) => {
-    const numero = parseInt(input);
-    
-for (let i = 1; i <= 10; i++) {
-    console.log(`${numero} x ${i} = ${numero * i}`);
-}
-  
-3 - 
-if (numero > 0) {
-    let soma = 0;
-    for (let i = 1; i <= numero; i++) {
-    soma += i;
-}
-    
-    console.log(`Resultado: ${soma}`);
-} else {
-    console.log("Por favor, digite um número maior que zero.");
-}
+function selecionarPorId(){
 
-INTERMEDIARIO
+const tituloFilme = document.getElementById('titulo-filme')
 
-1 - 
+console.log(tituloFilme)
+console.log(tituloFilme.tagName) //H3
+console.log(tituloFilme.textContent) //o conteudo do H3
+console.log(tituloFilme.id) //titulo-filme
 
-        for (int i = 2; i <= 50; i += 2) {
-    System.out.println(i);
-    }
-
-2 - 
-
-    int numeroSecreto = random.nextInt(100) + 1;
-    int palpite = 0;
-
-    System.out.println("Tente adivinhar o número entre 1 e 100!");
-
-    while (palpite != numeroSecreto) {
-    System.out.print("Digite seu palpite:");
-    palpite = scanner.nextInt();
-
-    if (palpite > numeroSecreto) {
-    System.out.println("O número secreto é menor.");
-
-    }else if (palpite < numeroSecreto) {
-    System.out.println("O número secreto é maior.");
-    }
-        }
-
-     System.out.println("Parabéns! Você acertou o número secreto!");
-    }
+//adicionei uma classe do elemento.
+tituloFilme.classList.add('elemento-selecionado')
 }
