535466deec14ccba6ee781c35e550bb882f779c7
Caue Tavares <caue.t.silva@edu.senai.br>
2026-09-04 15:57:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,3 +19,63 @@ console.log(tituloFilme.id) //titulo-filme
 //adicionei uma classe do elemento.
 tituloFilme.classList.add('elemento-selecionado')
 }
+
+function selecionarPorClasse(){
+
+    const elementos = document.getElementsByClassName("genero-acao")
+
+    console.log('Quantidade:' + elementos.length)
+
+    for (i = 0; i < elementos.length; i++){ 
+
+        console.log(elementos[i].textContent.trim())
+        elementos[i].classList.add('elemento-selecionado')
+
+
+    }
+}
+
+//QuerySelector - > retorna o PRIMEIRO elemento.
+
+function selecionarPorQuery() {
+
+    const elemento = document.querySelector('.filme-mini')
+    console.log(elemento)
+
+    elemento.classList.add('elemento-selecionado')
+    }
+
+    function selecionarTodos(){
+
+        const elementos = document.querySelectorAll('.filme-mini')
+
+        elementos.forEach( (el, idice) => {
+            console.log(Indice, el.textContent.trim())
+            el.classList.add('elemento-selecionado')
+            }
+        )
+    }
+
+    function mudarTexto(){ 
+
+        //selecionar o elemento que     ueremos mudar
+        const titulo = document.getElementById('titulo-destaque')
+
+        titulo.textContent = document.getElementById('input-busca').value
+
+        console.log('Novo Teste', titulo.textContent)
+    }
+
+    function adicionarBadge() {
+
+        const badge = document.createElement('span')
+
+        badge.className = 'badge'
+        badge.textContent = '⭐ Em Alta'
+
+        const container = document.getElementById('badge-container')
+
+        container.innerHTML = ''
+        
+        container.appendChild(badge)
+    }
